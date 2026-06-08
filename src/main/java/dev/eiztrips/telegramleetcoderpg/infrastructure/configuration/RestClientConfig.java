cb2f2a2457496121108ac5c10e2e1f9c0aebae4d
Eiztrips <eiztrips.dev@yandex.ru>
2026-06-08 18:05:43 +0300

    feat(config): add Redis & RestClient bean configs

---
+++ src/main/java/dev/eiztrips/telegramleetcoderpg/infrastructure/configuration/RestClientConfig.java
@@ -0,0 +1,21 @@
+package dev.eiztrips.telegramleetcoderpg.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
+
+@Configuration
+public class RestClientConfig {
+
+	@Bean
+	public RestClient.Builder restClientBuilder() {
+		var requestFactory = new SimpleClientHttpRequestFactory();
+		requestFactory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis());
+		requestFactory.setReadTimeout((int) Duration.ofSeconds(5).toMillis());
+
+		return RestClient.builder().requestFactory(requestFactory);
+	}
+}

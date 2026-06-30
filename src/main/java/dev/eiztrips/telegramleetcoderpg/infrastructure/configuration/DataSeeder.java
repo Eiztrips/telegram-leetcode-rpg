a7a64fbcaeba924623813d3533dee9c7e7fb6e13
Eiztrips <eiztrips.dev@yandex.ru>
2026-06-30 21:05:58 +0300

    chore: clear DataSeeder

--- src/main/java/dev/eiztrips/telegramleetcoderpg/infrastructure/configuration/DataSeeder.java
+++ src/main/java/dev/eiztrips/telegramleetcoderpg/infrastructure/configuration/DataSeeder.java
@@ -1,27 +1,9 @@
 package dev.eiztrips.telegramleetcoderpg.infrastructure.configuration;
 
-import dev.eiztrips.telegramleetcoderpg.domain.model.boss.WeeklyBoss;
-import dev.eiztrips.telegramleetcoderpg.infrastructure.adapters.outbound.persistence.boss.entity.WeeklyBossEntity;
-import dev.eiztrips.telegramleetcoderpg.infrastructure.adapters.outbound.persistence.boss.mapper.WeeklyBossMapper;
-import dev.eiztrips.telegramleetcoderpg.infrastructure.adapters.outbound.persistence.boss.repository.SpringDataBossRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("dev")
 public class DataSeeder {
-	@Bean
-	CommandLineRunner seedBoss(SpringDataBossRepository springDataBossRepository, WeeklyBossMapper weeklyBossMapper) {
-		return args -> {
-			if (springDataBossRepository.count() > 0)
-				return;
-
-			WeeklyBossEntity wbe = weeklyBossMapper
-					.toEntity(WeeklyBoss.builder().maxHp(100).currentHp(100).guildId(4689049129L).name("Фнюп").build());
-
-			springDataBossRepository.save(wbe);
-		};
-	}
 }
